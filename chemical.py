015d07d524e8e4c80fd5923d2eec41d81b7850fd
Arthur Barreto Morais <arthur.doutorado.ufjf@gmail.com>
2026-09-24 21:05:50 -0300

    Update and rename psml.py to chemical.py

---
+++ chemical.py
@@ -0,0 +1,53 @@
+import os
+import shutil
+
+def Obtain(fdf_file):
+    Species = {}
+    with open(fdf_file, 'r') as f:
+            in_block = False
+
+    for line in f:
+        line = line.strip()
+
+        if line == "%block ChemicalSpeciesLabel":
+            in_block = True
+            continue
+
+        if line == "%endblock ChemicalSpeciesLabel":
+            break
+
+        if in_block:
+            number, Z, symbol = line.split()
+            Species[symbol] = {"Number": int(number), "Z": int(Z)}
+    return Species
+
+def psml_find(fdf_file):
+    
+    dir = os.getcwd()
+    file_path = os.path.join(dir,fdf_file)
+    elements = []
+
+    in_block = False
+
+    with open(file_path,"r") as f:
+        line = f.read().strip().split("\n")
+        for info in line:
+            if "%block ChemicalSpeciesLabel" in info:
+                in_block = True
+                continue
+            
+            if "%endblock ChemicalSpeciesLabel" in info:
+                in_block = False
+                break
+
+            if in_block:
+                elements.append(info.strip().split()[-1])
+
+    dir_psml = os.path.expanduser("~/Siesta_Standart_psml/Scalar_Relativistic/")
+
+    for element in elements:
+
+        file_psml = os.path.join(dir_psml,f"{element}.psml")
+        
+        shutil.copy(file_psml, dir)
+    return
